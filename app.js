141483f56fff955a86bb70eb6e0b1f9189a5c7d8
Soumadip <39thdecember@gmail.com>
2025-02-06 23:49:09 +0530

    add custom Error-Handler

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import taskRoutes from "./routes/tasks.routes.js";
 import connectDb from "./db/connectDb.js";
 import notFound from "./middlewares/notFound.middleware.js";
+import errorHandler from "./middlewares/errorHandler.middleware.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -15,12 +16,9 @@ connectDb();
 app.use(express.static("./public"));
 app.use(express.json());
 
-// Routes
-// app.get("/", (_, res) => {
-//   res.send("Hello World from TASK API Home 🏠");
-// });
 app.use("/api/v1/tasks", taskRoutes);
 app.use(notFound); //404 Err.
+app.use(errorHandler); //custom Error Handler
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} 🛜`);
